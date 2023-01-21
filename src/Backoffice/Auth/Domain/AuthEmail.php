cd91651befa9749f24847d1671893017e698d78e
Gustavo Soliz <g.nel.soliz@gmail.com>
2023-01-21 19:09:13 -0300

    feat (user): add validation email

--- src/Backoffice/Auth/Domain/AuthEmail.php
+++ src/Backoffice/Auth/Domain/AuthEmail.php
@@ -3,7 +3,26 @@
 namespace Core\Backoffice\Auth\Domain;
 
 use Core\Shared\Domain\ValueObjects\StringValueObject;
+use InvalidArgumentException;
 
 final class AuthEmail extends StringValueObject
 {
+    public function __construct(string $value)
+    {
+        parent::__construct($value);
+
+        $this->ensureIsValidEmail($value);
+    }
+
+    private function ensureIsValidEmail(string $value): void
+    {
+        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    '"%s" is not a valid email address',
+                    $value
+                )
+            );
+        }
+    }
 }
